257a46bd085c9aa97cba338f467a41356a174c9a
DanielJCoetzer <djcoetzer10@gmail.com>
2022-09-26 14:47:33 +0200

    Added navigation bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,24 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <router-link to="/">
+          <b-navbar-brand href="/">Namify</b-navbar-brand>
+        </router-link>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">Shuffle</b-nav-item>
+          </b-navbar-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            </b-nav-form>
+            <b-nav-item href="#">Account</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
     <!-- Render the content of the current page view -->
     <router-view/>
