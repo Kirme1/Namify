f170fd64c851013fa9d0ca4623fc2b6f51732b95
Emrik Dunvald <edunvald@gmail.com>
2022-09-28 15:32:12 +0300

    Add names to home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,36 +1,128 @@
 <template>
   <div>
-    <b-jumbotron header="DIT342 Frontend" lead="Welcome to your DIT342 Frontend Vue.js App"></b-jumbotron>
-    <b-container>
-        <b-row>
-          <b-col>
-            <router-link to="name" tag="button">Names</router-link>
-          </b-col>
-          <b-col>
-            <router-link to="account" tag="button">Account</router-link>
-          </b-col>
-        </b-row>
-      </b-container>
+      <b-row>
+        <b-col>
+          <router-link to="name/likes" tag="button">Most Liked</router-link>
+          <div
+          v-for="name in mostLiked"
+          v-bind:key="name[1]._id">
+            <h1> {{name[1]._id}} </h1>
+            <p> {{"Likes: " + name[1].likes}} </p>
+          </div>
+        </b-col>
+        <b-col>
+          <router-link to="name/dislikes" tag="button">Most Disliked</router-link>
+          <div
+          v-for="name in mostDisliked"
+          v-bind:key="name[1]._id">
+            <h1> {{name[1]._id}} </h1>
+            <p> {{"Dislikes: " + name[1].dislikes}} </p>
+          </div>
+        </b-col>
+        <b-col>
+          <router-link to="name/controversial" tag="button">Controversial</router-link>
+          <div
+          v-for="name in mostDisliked"
+          v-bind:key="name[1]._id">
+            <h1> {{name[1]._id}} </h1>
+            <p> {{"Likes: " + name[1].likes}} </p>
+            <p> {{"Dislikes: " + name[1].dislikes}} </p>
+          </div>
+        </b-col>
+      </b-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { Api } from '@/Api'
+import { Api } from '../Api'
 // import nameItem from '@/components/nameItem.vue'
 
 export default {
-  name: 'names',
   data() {
     return {
-      message: 'none'
+      message: 'none',
+      mostLiked: {
+        nameAndLikes: [{ sortingLikes: 0 },
+          {
+            name:
+              {
+                comments: [],
+                tags: [],
+                _id: '',
+                likes: 0,
+                dislikes: 0,
+                __v: 0
+              }
+          }]
+      },
+      mostDisliked: {
+        nameAndLikes: [{ sortingLikes: 0 },
+          {
+            name:
+              {
+                comments: [],
+                tags: [],
+                _id: '',
+                likes: 0,
+                dislikes: 0,
+                __v: 0
+              }
+          }]
+      },
+      mostControversial: {
+        nameAndLikes: [{ sortingLikes: 0 },
+          {
+            name:
+              {
+                comments: [],
+                tags: [],
+                _id: '',
+                likes: 0,
+                dislikes: 0,
+                __v: 0
+              }
+          }]
+      }
     }
   },
+  mounted() {
+    this.getNamesByLikes()
+    this.getNamesByDislikes()
+    this.getNamesByControversial()
+  },
   methods: {
-    getMessage() {
-      Api.get('/names')
+    getNamesByLikes() {
+      Api.get('/names/sortLikes')
         .then(response => {
-          this.names = response
+          for (let i = 0; i < 5; i++) {
+            this.mostLiked[i] = response.data[i]
+          }
+          console.log(this.mostLiked)
+        })
+        .catch(error => {
+          this.message = error
+        })
+    },
+    getNamesByDislikes() {
+      Api.get('/names/sortDislikes')
+        .then(response => {
+          for (let i = 0; i < 5; i++) {
+            this.mostDisliked[i] = response.data[i]
+          }
+          console.log(this.mostDisliked)
+        })
+        .catch(error => {
+          this.message = error
+        })
+    },
+    getNamesByControversial() {
+      Api.get('/names/sortControversial')
+        .then(response => {
+          for (let i = 0; i < 5; i++) {
+            this.mostControversial[i] = response.data[i]
+          }
+          console.log(this.mostControversial)
         })
         .catch(error => {
           this.message = error
