e2ce9bbb101bd9ecb42e0e6596716ec201d7d7ef
Emrik Dunvald <edunvald@gmail.com>
2022-10-07 13:31:37 +0300

    home screen not working

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,30 +3,30 @@
       <b-row>
         <b-col>
           <router-link to="name/likes" tag="button">Most Liked</router-link>
-          <div
+          <p>{{this.mostLiked[1].name._id}}</p>
+          <li
           v-for="name in mostLiked"
-          v-bind:key="name[1]._id">
-            <h1> {{name[1]._id}} </h1>
-            <p> {{"Likes: " + name[1].likes}} </p>
-          </div>
+          v-bind:key="name._id">
+            <h1> {{name._id}} </h1>
+            <p> {{"Likes: " + name.likes}} </p>
+        </li>
         </b-col>
         <b-col>
           <router-link to="name/dislikes" tag="button">Most Disliked</router-link>
           <div
-          v-for="name in mostDisliked"
-          v-bind:key="name[1]._id">
-            <h1> {{name[1]._id}} </h1>
-            <p> {{"Dislikes: " + name[1].dislikes}} </p>
+          v-for="name in mostLiked"
+          v-bind:key="name._id">
+            <h1> {{name._id}} </h1>
+            <p> {{"Likes: " + name.likes}} </p>
           </div>
         </b-col>
         <b-col>
           <router-link to="name/controversial" tag="button">Controversial</router-link>
           <div
-          v-for="name in mostDisliked"
-          v-bind:key="name[1]._id">
-            <h1> {{name[1]._id}} </h1>
-            <p> {{"Likes: " + name[1].likes}} </p>
-            <p> {{"Dislikes: " + name[1].dislikes}} </p>
+          v-for="name in mostLiked"
+          v-bind:key="name._id">
+            <h1> {{name._id}} </h1>
+            <p> {{"Likes: " + name.likes}} </p>
           </div>
         </b-col>
       </b-row>
@@ -42,7 +42,17 @@ export default {
   data() {
     return {
       message: 'none',
-      mostLiked: {
+      mostLiked: [{
+        name: {
+          comments: [],
+          tags: [],
+          _id: '',
+          likes: 0,
+          dislikes: 0,
+          __v: 0
+        }
+      }],
+      mostDisliked: [{
         nameAndLikes: [{ sortingLikes: 0 },
           {
             name:
@@ -55,50 +65,34 @@ export default {
                 __v: 0
               }
           }]
-      },
-      mostDisliked: {
+      }],
+      mostControversial: [{
         nameAndLikes: [{ sortingLikes: 0 },
           {
             name:
               {
-                comments: [],
-                tags: [],
+                __v: 0,
                 _id: '',
-                likes: 0,
+                comments: [],
                 dislikes: 0,
-                __v: 0
+                likes: 0,
+                tags: []
               }
           }]
-      },
-      mostControversial: {
-        nameAndLikes: [{ sortingLikes: 0 },
-          {
-            name:
-              {
-                comments: [],
-                tags: [],
-                _id: '',
-                likes: 0,
-                dislikes: 0,
-                __v: 0
-              }
-          }]
-      }
+      }]
     }
   },
   mounted() {
     this.getNamesByLikes()
-    this.getNamesByDislikes()
-    this.getNamesByControversial()
   },
   methods: {
     getNamesByLikes() {
       Api.get('/names/sortLikes')
         .then(response => {
           for (let i = 0; i < 5; i++) {
-            this.mostLiked[i] = response.data[i]
+            this.mostLiked = response.data[i][1]
           }
-          console.log(this.mostLiked)
+          console.log(response.data)
         })
         .catch(error => {
           this.message = error
@@ -108,7 +102,7 @@ export default {
       Api.get('/names/sortDislikes')
         .then(response => {
           for (let i = 0; i < 5; i++) {
-            this.mostDisliked[i] = response.data[i]
+            this.mostDisliked[i].name = response.data[i]
           }
           console.log(this.mostDisliked)
         })
